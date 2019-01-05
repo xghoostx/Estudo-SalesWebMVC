421698df0d6d4c2c6c2cc6369c1c152e758637cf
Ewerson <vasconcelosdeveloper@outlook.com>
2019-01-05 00:52:27 -0200

    Layout das paginas do departments e seller modificados e iniciado o CRUD de seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -21,5 +21,10 @@ namespace SalesWebMVC.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
